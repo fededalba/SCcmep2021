8b383ffef2cbd9a2199a8409f6ddf1797af7851d
Federico D'Alba <f.dalba1@studenti.unipi.it>
2021-05-14 15:16:52 +0200

    initial import

---
+++ StarclassML/Dataunderstanding.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 14 14:44:09 2021
+
+@author: Uno
+"""
+
+
+import math
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+
+    df = pd.read_csv('C:\\Users\\Uno\\Documents\\Uni\\Computing methods\\Esame\\Stars.csv')
+
+
