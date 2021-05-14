eb514caca3fe1043360706461694c1c9ac854a6b
Federico D'Alba <f.dalba1@studenti.unipi.it>
2021-05-14 15:17:07 +0200

    inital import

---
+++ StarclassML/Decisiontreeclassifier.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 14 14:53:44 2021
+
+@author: Uno
+"""
+
+import math
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+def into_a_string(x):
+    ''' Prendo una lista di valori e trasformo ogni elemento in stringa'''
+    new_labels = [str(element) for element in x]
+    return(new_labels)
+
+if __name__ == '__main__':
+
+    df = pd.read_csv('C:\\Users\\Uno\\Documents\\Uni\\Computing methods\\Esame\\Stars.csv')
+
+    ##Trasformo la variabile target in una stringa.
+    labels = df['Type'].values
+    del df['Type']
+
+    df['Type'] = into_a_string(labels)
+
+
+
+    ##Mappo i valori categorici in numeri interi.
+    from sklearn.preprocessing import LabelEncoder
+    label_encoders = dict()
+    column2encode = ['Spectral_Class', 'Color']
+    for col in column2encode:
+        le = LabelEncoder()
+        df[col] = le.fit_transform(df[col])
+        label_encoders[col] = le
+
+
